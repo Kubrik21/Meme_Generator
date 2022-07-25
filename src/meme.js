1d6a86a28f55c6418bc2ad483c2aacbfef187772
Ciemny21 <jakub.olejnik@student.wat.edu.pl>
2022-07-25 18:45:52 +0200

    useState

--- src/meme.js
+++ src/meme.js
@@ -1,12 +1,26 @@
+import React from "react";
 import "./meme.css"
+import {Memy} from "./memes"
+import {useState} from "react"
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+    } 
+
+    
+
+
 function Meme(){
+    const [image,setImage]=useState("non");
+    
 return(
     <main>
         <div className="input">
             <input type="text" placeholder="Wpisz tekst u góry"></input>
             <input type="text" placeholder="Wpisz tekst u dołu"></input>
         </div>
-        <button className="buttom">Generuj Mem!</button>
+        <button className="buttom" onClick={()=>setImage(Memy.data.memes[getRndInteger(0,1)])}>Generuj Mem!</button>
+        {image!=="non"&&<img src={image.url} alt="meme" width={image.width} height={image.height} className="meme"></img>}
     </main>
 
 )
---
+++ src/memes.js
@@ -0,0 +1,25 @@
+const Memy={
+"succes":true,
+"data":{
+    "memes":[
+        {
+            "id":1,
+            "name":"angry boy",
+            "url":"./IMG/meme-amgry.jpg",
+            "width":800,
+            "height":600,
+            "box_count":1
+        },
+        {
+            "id":2,
+            "name":"best friend",
+            "url":"./IMG/best-friend.jpg",
+            "width":800,
+            "height":600,
+            "box_count":2
+        }        
+    ]
+}
+
+}
+export {Memy}
