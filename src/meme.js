71cfb71bb02298dee57b97d86a9eee5667d18499
Ciemny21 <jakub.olejnik@student.wat.edu.pl>
2022-07-26 19:04:14 +0200

    Rendering text on Pics

--- src/meme.js
+++ src/meme.js
@@ -12,15 +12,28 @@ function getRndInteger(min, max) {
 
 function Meme(){
     const [image,setImage]=useState("non");
+    const [firstText,setFirstText]=useState('');
+    const [seccondText,setSeccondText]=useState('');
+    const [text,setText]=useState(['',''])
+
+function Create_text()
+{
+    setText(elem=>[firstText, seccondText])
+}
     
 return(
     <main>
         <div className="input">
-            <input type="text" placeholder="Wpisz tekst u góry"></input>
-            <input type="text" placeholder="Wpisz tekst u dołu"></input>
+            <input type="text" placeholder="Wpisz tekst u góry" onChange={elem=>setFirstText(elem.target.value)}></input>
+            <input type="text" placeholder="Wpisz tekst u dołu" onChange={elem=>setSeccondText(elem.target.value)}></input>
         </div>
-        <button className="buttom" onClick={()=>setImage(Memy.data.memes[getRndInteger(0,1)])}>Generuj Mem!</button>
-        {image!=="non"&&<img src={image.url} alt="meme" width={image.width} height={image.height} className="meme"></img>}
+        <button className="buttom" onClick={()=>setImage(Memy.data.memes[getRndInteger(0,1)])}>Zmień obrazek!</button>
+        <button className="buttom" onClick={()=>Create_text()}>Generuj Mem!</button>
+        <div className="meme-element">
+            {text[0]!==''&&<p className="meme-text-top">{text[0]}</p>}
+            {image!=="non"&&<img src={image.url} alt="meme" width={image.width} height={image.height} className="meme"></img>}
+            {text[1]!==''&&<p className="meme-text-bottom">{text[1]}</p>}
+        </div>
     </main>
 
 )
